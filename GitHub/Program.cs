23f58eccfdfaeb6b37b4f9687e1f92b85fe796e8
Max <Max@DESKTOP-UOR88NM>
2022-06-29 18:50:26 +0300

    Добавьте файлы проекта.

---
+++ GitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\tHELLO WORLD\n");
+            Console.ReadKey();
+        }
+    }
+}
