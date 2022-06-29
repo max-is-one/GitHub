a7b7036c1278fe9c30b064dad249cb0757093c93
want-pizza <olex2005my@gmail.com>
2022-06-29 21:02:23 +0300

    Created FishSpawner

--- GitHub/Program.cs
+++ GitHub/Program.cs
@@ -8,14 +8,52 @@ namespace GitHub
 {
     internal class Program
     {
+        static void AddFishs(ref char[,] lake)
+        {
+            char[] fishs = new char[5];
+            Random r = new Random();
+            int value;
+            for(int i = 0; i < 4; i++)
+            {
+                value = r.Next(100);
+
+                if (value < 30)
+                {
+                    fishs[i] = ' ';
+                }else if(value>=30 && value <= 69)
+                {
+                    fishs[i] = 'm';
+                }else if(value>=70 && value <= 89)
+                {
+                    fishs[i] = 'b';
+                }
+                else
+                {
+                    fishs[i] = 's';
+                }
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if(lake[i,j]!=' ')
+                    {
+                        lake[i, j] = fishs[i];
+                    }
+                }
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("\n\tHELLO WORLD\n");
-            Console.WriteLine("\n\tHELLO WORLD\n");
-            Console.ReadKey();
-            //Accept
-            //o_O
-            //commit
+            Console.WriteLine("ExploedFish");
+            char[] column = new char[] { '|', '1', '|', '2', '|', '3', '|', '4', '|', '5', '|' };
+            char[,] lake = new char[5, 5];
+            for(int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                    lake[i, j] = ' ';
+            }
+            
         }
     }
 }
